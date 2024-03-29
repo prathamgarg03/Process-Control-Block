6ff726bb40392cf33dbc5ab32e18825cc296cf93
prathamgarg03 <89456233+prathamgarg03@users.noreply.github.com>
2024-03-28 20:29:50 -0700

    Update helper.c

--- helper.c
+++ helper.c
@@ -10,7 +10,6 @@ int compareID(void * pItem, void * pComparisonArg) {
     }
     return 0;
 }
-
 bool isProcessFound(List * list, int id) {
     COMPARATOR_FN comparator = (COMPARATOR_FN) &compareID;
     if(List_search(list, comparator, (void *)&id)) {
@@ -51,6 +50,23 @@ char * freeMessage(MessagePacket * msg) {
     return receivedMessage;
 }
 
+int compareSemaphore(void * pItem, void * pComparisonArg) {
+    Semaphore * ID1 = (Semaphore *)pItem;
+    int * ID2 = (int *) pComparisonArg;
+    if(ID1->ID == *ID2) {
+        return 1;
+    }
+    return 0;
+}
+
+bool isSemaphoreFound(List * list, int id) {
+    COMPARATOR_FN comparator = (COMPARATOR_FN) &compareSemaphore;
+    if(List_search(list, comparator, (void *)&id)) {
+        return true;
+    }
+    return false;
+}
+
 void printList(char * listName, List * list) {
     printf("%s list: [", listName);
     if(List_count(list) == 0) {
@@ -66,4 +82,50 @@ void printList(char * listName, List * list) {
         printf(" %d ", it->ID);
     }
     printf("]\n");
+}
+
+void printInfo(PCB * process, int i) {
+    char * state[] = {
+            "RUNNING",
+            "READY",
+            "BLOCKED"
+    };
+    char * priority[] = {
+            "RUNNING",
+            "READY",
+            "BLOCKED"
+    };
+    printf("PID: %d \n", process->ID);
+    printf("Process state: %s \n", state[process->state]);
+    printf("Process priority: %s \n", priority[process->priority]);
+    if(process->message) {
+        printf("Process message: %s \n", process->message);
+    } else {
+        printf("No message.");
+    }
+    char * listName = "No List";
+    switch (i) {
+        case 1:
+            listName = "HIGH priority ready list\n";
+            break;
+        case 2:
+            listName = "NORMAL priority ready list\n";
+            break;
+        case 3:
+            listName = "LOW priority ready list\n";
+            break;
+        case 4:
+            listName = "sender list\n";
+            break;
+        case 5:
+            listName = "receiver list\n";
+            break;
+        case 6:
+            listName = "semaphore list\n";
+            break;
+        default:
+            listName = "no list\n";
+            break;
+    }
+    printf("Process list: %s", listName);
 }
